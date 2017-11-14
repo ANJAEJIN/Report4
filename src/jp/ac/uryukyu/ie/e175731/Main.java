08c3a555dd9b4987a687ef7b9c26ebe32e3d4a0c
AN JAEJIN <e175731@ie.u-ryukyu.ac.jp>
2017-11-14 17:08:32 +0900

    テップ3: APIリファレンスを参照してみよう。

--- src/jp/ac/uryukyu/ie/e175731/Main.java
+++ src/jp/ac/uryukyu/ie/e175731/Main.java
@@ -3,9 +3,9 @@ package jp.ac.uryukyu.ie.e175731;
 public class Main {
     public static void main(String[] args){
         try{
-            String str = null;
-            System.out.println(str.length());
-        }catch (NullPointerException e){
+            String str = "3．14";
+            double d = Double.parseDouble(str);
+        }catch (NumberFormatException e){
             System.out.println(e.toString());
             System.out.println(e.getMessage());
         }
